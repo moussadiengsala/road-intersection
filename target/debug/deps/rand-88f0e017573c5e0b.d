2d46b90a3268bc3404361fee23e73f4ebcbd02fd
mousdieng <moizadieng@gmail.com>
2024-05-13 19:04:31 +0000

    init

--- target/debug/deps/rand-88f0e017573c5e0b.d
+++ target/debug/deps/rand-88f0e017573c5e0b.d
@@ -1,6 +1,6 @@
-/home/moussa/road_intersection/target/debug/deps/librand-88f0e017573c5e0b.rmeta: /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/lib.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/distributions/mod.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/rand_impls.rs
+/home/moussa/road-intersection/target/debug/deps/librand-88f0e017573c5e0b.rmeta: /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/lib.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/distributions/mod.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/rand_impls.rs
 
-/home/moussa/road_intersection/target/debug/deps/rand-88f0e017573c5e0b.d: /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/lib.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/distributions/mod.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/rand_impls.rs
+/home/moussa/road-intersection/target/debug/deps/rand-88f0e017573c5e0b.d: /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/lib.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/distributions/mod.rs /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/rand_impls.rs
 
 /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/lib.rs:
 /home/moussa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand-0.3.23/src/distributions/mod.rs:
--- target/debug/deps/road_intersection-9d29624d3cee3c53.d
+++ target/debug/deps/road_intersection-9d29624d3cee3c53.d
@@ -1,6 +1,6 @@
-/home/moussa/road_intersection/target/debug/deps/libroad_intersection-9d29624d3cee3c53.rmeta: src/main.rs src/map.rs src/traffic.rs src/cars.rs src/lane.rs
+/home/moussa/road-intersection/target/debug/deps/libroad_intersection-9d29624d3cee3c53.rmeta: src/main.rs src/map.rs src/traffic.rs src/cars.rs src/lane.rs
 
-/home/moussa/road_intersection/target/debug/deps/road_intersection-9d29624d3cee3c53.d: src/main.rs src/map.rs src/traffic.rs src/cars.rs src/lane.rs
+/home/moussa/road-intersection/target/debug/deps/road_intersection-9d29624d3cee3c53.d: src/main.rs src/map.rs src/traffic.rs src/cars.rs src/lane.rs
 
 src/main.rs:
 src/map.rs:
